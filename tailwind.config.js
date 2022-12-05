82f1527f8261dc92575ca8e88f7dc8b4db56244b
Triin Trempi <tri0022@stud.kea.dk>
2022-12-05 20:31:04 +0100

    header, hero section, card components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,42 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: "640px",
+
+      md: "1150px",
+
+      lg: "1200px",
+
+      xl: "1400px",
+
+      "2xl": "1455px",
+    },
     extend: {
-      colors: {},
+      colors: {
+        primary: "#3295ED",
+        secondary: "#45473B",
+        background: "#DBEAFE",
+        blackshade: "#272725",
+      },
+
+      fontSize: {
+        sm: ".75rem",
+        md: "1rem",
+        lg: "1.25rem",
+        xl: "2.25rem",
+        "2xl": "2rem",
+      },
+
       fontFamily: {
         lato: "'Lato', sans-serif",
+        montserrat: "'Montserrat', sans-serif",
       },
     },
     plugins: [
       require("@tailwindcss/typography"),
       require("@tailwindcss/forms"),
-      require("daisyui"),
     ],
-
-    daisyui: {
-      styled: true,
-      themes: true,
-      base: true,
-      utils: true,
-      logs: true,
-      rtl: false,
-      prefix: "",
-      darkTheme: "dark",
-    },
   },
+  plugins: [require("daisyui")],
 };
